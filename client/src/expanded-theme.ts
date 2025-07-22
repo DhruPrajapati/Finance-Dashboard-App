e55ade5b23fd58d3eb9ccdc9faad432cea6268d7
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2025-07-21 20:22:45 -0400

    row to commit

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,9 +1,9 @@
-// theme.d.ts (or any global `.ts` file loaded during compile)
-import "@mui/material/styles";
-
 declare module "@mui/material/styles" {
   interface PaletteColor {
-    [key: number]: string; // ✅ allow primary[100], etc.
-    tertiary?: string;
+    [key: number]: string;
+  }
+
+  interface Palette {
+    tertiary: PaletteColor;
   }
 }
