800c20150ae18326109825e2fbfe52b87dfd8b5b
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2025-07-26 17:23:14 -0400

    changing title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "@/scenes/navbar";
 import Dashboard from "./scenes/dashboard";
-
+import Predictions from "./scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,11 +15,10 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-
-          <Navbar/>
+          <Navbar />
           <Routes>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/predictions" element={<div>prediction pages</div>} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/predictions" element={<Predictions />} />
           </Routes>
         </Box>
       </ThemeProvider>
